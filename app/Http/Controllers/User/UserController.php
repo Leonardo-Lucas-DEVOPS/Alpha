9d3fb940b9329461b9ef51e5c0add171eb4f1308
Leonardo-Lucas-SPO <Leonardolucas181204@gmail.com>
2024-05-19 00:44:31 -0300

    construindo layout para usuario comun

--- app/Http/Controllers/User/UserController.php
+++ app/Http/Controllers/User/UserController.php
@@ -8,7 +8,7 @@ use Illuminate\Http\Request;
 class UserController extends Controller
 {
     public function index(){
-        return view('dashboard');
+        return view('user.dashboard');
     }
     
 }
