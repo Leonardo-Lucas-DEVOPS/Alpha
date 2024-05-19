c0910e45fe9ca0b855c47176ec27e1fcd61a8fca
Leonardo-Lucas-SPO <Leonardolucas181204@gmail.com>
2024-05-19 00:45:15 -0300

    Preciso terminar o layout mobile usuario comun

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -3,7 +3,8 @@
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div class="flex justify-between h-16">
             <div class="flex">
-                <!-- Logo -->
+
+            <!-- Logo -->
                 <div class="shrink-0 flex items-center">
                     @if (Auth::user()->usertype =="3")
                     <a href="{{ route('dashboard') }}">
@@ -21,26 +22,73 @@
 
                 </div>
 
-                <!-- Navigation Links -->
-                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                    @if (Auth::user()->usertype =="3")
-                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                        {{ __('Dashboard') }}
-                    </x-nav-link>
-                    @elseif (Auth::user()->usertype =="2")
-                    <x-nav-link :href="route('admin.dashboard')" :active="request()->routeIs('admin.dashboard')">
-                        {{ __('Dashboard') }}
-                    </x-nav-link>
-                    @elseif (Auth::user()->usertype =="1")
-                    <x-nav-link :href="route('super.admin.dashboard')" :active="request()->routeIs('super.admin.dashboard')">
-                        {{ __('Dashboard') }}
-                    </x-nav-link>
-                    @endif
+
+                <!-- Dropdown Consultas Web -->
+                <div class="hidden sm:flex sm:items-center sm:ms-6">
+                    <x-dropdown align="right" width="48">
+                        <x-slot name="trigger">
+                            <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150">
+                                {{ __('Queries') }}
+                                <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                                    <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
+                                </svg>
+                            </button>
+                        </x-slot>
+
+                        <x-slot name="content">
+                            <!-- Opções de Consultas -->
+                            <x-dropdown-link :href="route('dashboard')">
+                                Consulta Carro
+                            </x-dropdown-link>
+
+                            <x-dropdown-link :href="route('dashboard')">
+                                Consulta Funcionário
+                            </x-dropdown-link>
+
+                            <x-dropdown-link :href="route('dashboard')">
+                                Consulta Agregado
+                            </x-dropdown-link>
+                        </x-slot>
+                    </x-dropdown>
+                </div>
+
+
+                <!-- Dropdown relatorios Web -->
+                <div class="hidden sm:flex sm:items-center sm:ms-6">
+                    <x-dropdown align="right" width="48">
+                        <x-slot name="trigger">
+                            <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150">
+                                {{__('Queries')}} 
+                                <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                                    <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
+                                </svg>
+                            </button>
+                        </x-slot>
+
+                        <x-slot name="content">
+                            <!-- Opções de Relatórios -->
+                            <x-dropdown-link :href="route('dashboard')">
+                                Relatório de Funcionários
+                            </x-dropdown-link>
+
+                            <x-dropdown-link :href="route('dashboard')">
+                                Relatório de Carros
+                            </x-dropdown-link>
+
+                            <x-dropdown-link :href="route('dashboard')">
+                                Relatório de Agregados
+                            </x-dropdown-link>
+                        </x-slot>
+                    </x-dropdown>
                 </div>
 
             </div>
 
-            <!-- Settings Dropdown -->
+
+
+
+
+            <!-- Settings Dropdown Web -->
             <div class="hidden sm:flex sm:items-center sm:ms-6">
                 <x-dropdown align="right" width="48">
                     <x-slot name="trigger">
@@ -73,6 +121,8 @@
                 </x-dropdown>
             </div>
 
+
+            
             <!-- Hamburger -->
             <div class="-me-2 flex items-center sm:hidden">
                 <button @click="open = ! open" class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out">
@@ -95,15 +145,27 @@
 
         <!-- Responsive Settings Options -->
         <div class="pt-4 pb-1 border-t border-gray-200">
-            <div class="px-4">
-                <div class="font-medium text-base text-gray-800">{{ Auth::user()->name }}</div>
-                <div class="font-medium text-sm text-gray-500">{{ Auth::user()->email }}</div>
-            </div>
-
+            
             <div class="mt-3 space-y-1">
                 <x-responsive-nav-link :href="route('profile.edit')">
                     {{ __('Profile') }}
                 </x-responsive-nav-link>
+
+
+            <!--terminar esse trecho -->
+
+                
+                <x-responsive-nav-link :href="route('profile.edit')">
+                    {{ __('Queries') }}
+                </x-responsive-nav-link>
+
+                <x-responsive-nav-link :href="route('profile.edit')">
+                    {{ __('Reports') }}
+                </x-responsive-nav-link>
+
+
+
+
 
                 <!-- Authentication -->
                 <form method="POST" action="{{ route('logout') }}">
