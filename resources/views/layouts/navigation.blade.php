e441895ce5e2c0edd5ce3d5bbc2f2ef9743cb13b
Leonardo-Lucas-SPO <Leonardolucas181204@gmail.com>
2024-05-24 11:34:57 -0300

    Layout Web with multi access

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -4,7 +4,7 @@
         <div class="flex justify-between h-16">
             <div class="flex">
 
-            <!-- Logo -->
+                <!-- Logo -->
                 <div class="shrink-0 flex items-center">
                     @if (Auth::user()->usertype =="3")
                     <a href="{{ route('dashboard') }}">
@@ -19,8 +19,87 @@
                         <x-application-logo class="block h-9 w-auto fill-current text-gray-800" />
                     </a>
                     @endif
-
                 </div>
+
+                <!-- Dropdown Empresas Web -->
+                
+                @if(Auth::User()->usertype =="1")
+                <div class="hidden sm:flex sm:items-center sm:ms-6">
+                    <x-dropdown align="right" width="48">
+                        <x-slot name="trigger">
+                            <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150">
+                                {{ __('Companies') }}
+                                <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                                    <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
+                                </svg>
+                            </button>
+                        </x-slot>
+                        <x-slot name="content">
+                            <x-dropdown-link :href="route('dashboard')">
+                                Nova Empresa
+                            </x-dropdown-link>
+
+                            <x-dropdown-link :href="route('dashboard')">
+                                Gerenciar Empresas
+                            </x-dropdown-link>
+                        </x-slot>
+                    </x-dropdown>
+                </div>
+                @endif
+
+                <!-- Dropdown Afiliado Web -->
+                @if(Auth::User()->usertype <="2")
+                <div class="hidden sm:flex sm:items-center sm:ms-6">
+                    <x-dropdown align="right" width="48">
+                        <x-slot name="trigger">
+                            <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150">
+                                {{ __('affiliates') }}
+                                <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                                    <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
+                                </svg>
+                            </button>
+                        </x-slot>
+                        <x-slot name="content">
+                            <x-dropdown-link :href="route('dashboard')">
+                                Novo Afiliado
+                            </x-dropdown-link>
+
+                            <x-dropdown-link :href="route('dashboard')">
+                                Gerenciar Afiliados
+                            </x-dropdown-link>
+                        </x-slot>
+                    </x-dropdown>
+                </div>
+                @endif
+
+                <!-- Dropdown Relatório Financeiro Web -->
+                @if(Auth::User()->usertype <="2")
+                <div class="hidden sm:flex sm:items-center sm:ms-6">
+                    <x-dropdown align="right" width="48">
+                        <x-slot name="trigger">
+                            <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150">
+                                {{ __('Financial Reports') }}
+                                <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                                    <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
+                                </svg>
+                            </button>
+                        </x-slot>
+
+                        <x-slot name="content">
+                            
+                        @if(Auth::User()->usertype <="1")
+                            <x-dropdown-link :href="route('dashboard')">
+                                Relatório Financeiro Geral
+                            </x-dropdown-link>
+                        @endif
+
+                            <x-dropdown-link :href="route('dashboard')">
+                                Relatório Financeiro 
+                            </x-dropdown-link>
+                        </x-slot>
+                    </x-dropdown>
+                </div>
+                @endif
 
 
                 <!-- Dropdown Consultas Web -->
@@ -34,7 +113,6 @@
                                 </svg>
                             </button>
                         </x-slot>
-
                         <x-slot name="content">
                             <!-- Opções de Consultas -->
                             <x-dropdown-link :href="route('dashboard')">
@@ -58,13 +136,12 @@
                     <x-dropdown align="right" width="48">
                         <x-slot name="trigger">
                             <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150">
-                                {{__('Queries')}} 
+                                {{__('Reports')}}
                                 <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
                                     <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
                                 </svg>
                             </button>
                         </x-slot>
-
                         <x-slot name="content">
                             <!-- Opções de Relatórios -->
                             <x-dropdown-link :href="route('dashboard')">
@@ -78,15 +155,17 @@
                             <x-dropdown-link :href="route('dashboard')">
                                 Relatório de Agregados
                             </x-dropdown-link>
+
+                            @if(Auth::User()->usertype =="1")
+                            <x-dropdown-link :href="route('dashboard')">
+                                Gerenciar Relatórios
+                            </x-dropdown-link>
+                            @endif
+
                         </x-slot>
                     </x-dropdown>
                 </div>
-
             </div>
-
-
-
-
 
             <!-- Settings Dropdown Web -->
             <div class="hidden sm:flex sm:items-center sm:ms-6">
@@ -122,7 +201,7 @@
             </div>
 
 
-            
+
             <!-- Hamburger -->
             <div class="-me-2 flex items-center sm:hidden">
                 <button @click="open = ! open" class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out">
@@ -145,16 +224,16 @@
 
         <!-- Responsive Settings Options -->
         <div class="pt-4 pb-1 border-t border-gray-200">
-            
+
             <div class="mt-3 space-y-1">
                 <x-responsive-nav-link :href="route('profile.edit')">
                     {{ __('Profile') }}
                 </x-responsive-nav-link>
 
 
-            <!--terminar esse trecho -->
+                <!--terminar esse trecho -->
 
-                
+
                 <x-responsive-nav-link :href="route('profile.edit')">
                     {{ __('Queries') }}
                 </x-responsive-nav-link>
